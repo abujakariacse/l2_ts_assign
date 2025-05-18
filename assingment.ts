a0325705b72fad9a53284b095e8966ec3206ec24
Abu Jakaria <abujakaria316@gmail.com>
2025-05-19 04:04:24 +0600

    fourth problem solved

--- assingment.ts
+++ assingment.ts
@@ -12,3 +12,27 @@ function filterByRating(items: Book[]): Book[] {
   const filteredBooks: Book[] = items.filter((item: Book) => item.rating >= 4);
   return filteredBooks;
 }
+
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+  getInfo() {
+    console.log(`Make: ${this.make}, Year: ${this.year}`);
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+  getModel() {
+    console.log(`Model: ${this.model}`);
+  }
+}
