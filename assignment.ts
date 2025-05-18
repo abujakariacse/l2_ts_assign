9ea51d523450c8f58be9c4e6c904a5b4736cb294
Abu Jakaria <abujakaria316@gmail.com>
2025-05-19 04:44:39 +0600

    seventh problem solved

--- assignment.ts
+++ assignment.ts
@@ -64,6 +64,24 @@ function getMostExpensiveProduct(products: Product[]): Product | null {
   return mostExpensive;
 }
 
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  if (day === Day.Saturday || day === Day.Sunday) {
+    return "Weekend";
+  } else {
+    return "Weekday";
+  }
+}
+
 async function squareAsync(n: number): Promise<number> {
   return new Promise<number>((resolve, reject) => {
     if (n > 0) {
