9eb4c2cecc5e60b7e8830b0192f2b2a3377104b0
Abu Jakaria <abujakaria316@gmail.com>
2025-05-19 03:48:04 +0600

    second problem solved

--- assingment.ts
+++ assingment.ts
@@ -5,3 +5,10 @@ function formatString(input: string, toUpper?: boolean): string {
     return input.toLowerCase();
   }
 }
+
+type Book = { title: string; rating: number };
+
+function filterByRating(items: Book[]): Book[] {
+  const filteredBooks: Book[] = items.filter((item: Book) => item.rating >= 4);
+  return filteredBooks;
+}
