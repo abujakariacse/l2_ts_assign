9d6f1bcfae4cb0b598d31975e73de11671d8a5d9
Abu Jakaria <abujakaria316@gmail.com>
2025-05-19 03:41:13 +0600

    solved first problem

---
+++ assingment.ts
@@ -0,0 +1,7 @@
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper || toUpper == undefined) {
+    return input.toUpperCase();
+  } else {
+    return input.toLowerCase();
+  }
+}
