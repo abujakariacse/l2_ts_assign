3f6e6e06ae220ddbc35cf726d6ece85d0d4b6633
Abu Jakaria <abujakaria316@gmail.com>
2025-05-19 04:33:31 +0600

    eighth problem solved

--- assingment.ts
+++ assingment.ts
@@ -59,3 +59,15 @@ function getMostExpensiveProduct(products: Product[]): Product | null {
   }
   return mostExpensive;
 }
+
+async function squareAsync(n: number): Promise<number> {
+  return new Promise<number>((resolve, reject) => {
+    if (n > 0) {
+      setTimeout(() => {
+        resolve(n * n);
+      }, 1000);
+    } else {
+      reject("Negative number not allowed");
+    }
+  });
+}
